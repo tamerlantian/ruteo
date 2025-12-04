c8ae3bd662f68d90ddf163c3b24c777dd79c45f3
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2025-12-04 10:01:22 -0500

    refactor: update iOS bundle identifier and module name from "ruteo" to "lutencio"
    - Changed PRODUCT_BUNDLE_IDENTIFIER from com.semantica.ruteo to com.anonymous.lutencio for both Debug and Release configurations
    - Updated React Native module name from "ruteo" to "lutencio" in AppDelegate.swift

--- ios/ruteo/AppDelegate.swift
+++ ios/ruteo/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "ruteo",
+      withModuleName: "lutencio",
       in: window,
       launchOptions: launchOptions
     )
