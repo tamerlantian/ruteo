559e7e5a862701b2bc1f34546e062d314a104ebc
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2025-12-04 10:00:01 -0500

    refactor: rebrand Android app from "ruteo" to "lutencio" and configure release signing
    - Changed package namespace from com.ruteo to com.anonymous.lutencio
    - Updated applicationId to com.anonymous.lutencio
    - Bumped versionCode to 39 and versionName to 9.18.0
    - Added release signing configuration with keystore properties
    - Moved MainActivity and MainApplication to new package structure
    - Updated main component name from "ruteo" to "lutencio"
    - Added Google Ads ID permission and opt-out metadata to

--- android/app/src/main/java/com/anonymous/lutencio/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/lutencio/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ruteo
+package com.anonymous.lutencio
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ruteo"
+  override fun getMainComponentName(): String = "lutencio"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/anonymous/lutencio/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/lutencio/MainApplication.kt
@@ -1,6 +1,7 @@
-package com.ruteo
+package com.anonymous.lutencio
 
 import android.app.Application
+import com.anonymous.lutencio.BuildConfig
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
